51fae87e84715adad88a068aa2d576b33914e240
Mohammad Arslan <arslan48950@gmail.com>
2026-07-27 23:02:41 +0530

    Fixed vite proxy server crash

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,18 +1,22 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-const backend_url = new URL(import.meta.env.VITE_BACKEND_URL).origin ;
+export default defineConfig(({ mode }) => {
+  
+  const env = loadEnv(mode, process.cwd(), '');  
+  const backend_url = new URL(env.VITE_BACKEND_URL).origin;
 
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  server: {
-    port: 5174,
-    strictPort: true,
-    proxy: {
-      '/api': {
-        target: backend_url,
-        changeOrigin: true,
+  return {
+    plugins: [react(), tailwindcss()],
+    server: {
+      port: 5174,
+      strictPort: true,
+      proxy: {
+        '/api': {
+          target: backend_url,
+          changeOrigin: true,
+        }
       }
     }
   }
